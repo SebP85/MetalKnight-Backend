21b3f6b176177b088fbb34362722f2ebafc685f0
SebP85 <sebastien.picot2@gmail.com>
2021-04-21 22:00:49 +0200

    valide données d'entrées

--- controllers/checkBody.js
+++ controllers/checkBody.js
@@ -2,18 +2,23 @@
  * Vérifie les données d'entrées de la requête
  */
 
-const validateRequest = require('express-body-validator');
-const handleError = require('../lib/handleError');
-
-const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,6}$/i;
+ const { body, validationResult } = require('express-validator');
 
 exports.register = function (req, res) {
-    validateRequest(req, [
-        { name: 'email', type: 'string', validator: v => v.pattern(emailRegex), failMsg: 'email must be an email string' },
-        { name: 'password', type: 'string', validator: v => v.minLength(8).maxLength(50), failMsg: 'password must be comprised between 8 and 50 chars' }
-    ])
-    //.then(post => userModel.create(post.email, post.password))
-    //.then(([id, token]) => res.status(201).json({ id, token }))
-    .then( () => next())
-    .catch(err => handleError(err, res));
+    // username must be an email
+    body('username').isEmail(),
+    // password must be at least 5 chars long
+    body('password').isLength({ min: 5 }),
+    (req, res) => {
+        // Finds the validation errors in this request and wraps them in an object with handy functions
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        } else {
+            next();
+        }
+    }
 };
+
+
+//////faire les autres
